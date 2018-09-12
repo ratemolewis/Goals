9f18342c2a14082cc35810c2210cf5db23ac6297
ratemo <ratemolewis@gmail.com>
2018-09-12 10:39:08 -0400

    Addition of Goals App

---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import {Goal} from './goal'
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html'
+})
+export class AppComponent {
+  //   goals: string[];
+  //
+  // constructor(){
+  //
+  //   this.goals = ["Watch Finding Nemo","Buy Cookies","Get new phone case"];
+  // }
+  //removed to goal/goal.component.ts
+  // goals = [
+  //       new Goal(1,'Watch Finding Nemo'),
+  //       new Goal(2,'Buy Cookies'),
+  //       new Goal(3, 'Get new Phone Case'),
+  //       new Goal(4, 'Get Dog Food'),
+  //       new Goal(5, 'Solve math homework'),
+  //       new Goal(6, 'Plot my world domination plan')
+  //
+  //   ]
+}
